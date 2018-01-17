74c703515f0442b0c31838da80343c1c324e5baf
olya-qznecova <35519547+olya-qznecova@users.noreply.github.com>
2018-01-17 13:34:31 +0300

    Add files via upload

---
+++ Тестовое/new.js
@@ -0,0 +1,13 @@
+
+$.fn.animate_Text = function() {
+    var string = this.text();
+    return this.each(function(){
+        var $this = $(this);
+        $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+        $this.find('span.new').each(function(i, el){
+            setTimeout(function(){ $(el).addClass('div_opacity'); }, 150 * i);
+        });
+    });
+};
+$('#example').show();
+$('#example').animate_Text();
